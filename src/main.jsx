a8e71fa32350e7df9e2affba37029fc2e7cf8984
IndranilPaul15 <indranil.paul04@gmail.com>
2025-06-20 01:33:29 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { StrictMode } from 'react'
 import './index.css'
 import './App.css';
@@ -8,8 +8,8 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/quizopedia">
+    <HashRouter  basename="/quizopedia">
       <App />
-    </BrowserRouter>
+    </HashRouter >
   </StrictMode>,
 )
