be14fb14e6cccd4f05eadba8cedbf1b00a8111c1
IndranilPaul15 <indranil.paul04@gmail.com>
2025-06-20 01:38:44 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter  basename="/quizopedia">
+    <HashRouter  >
       <App />
     </HashRouter >
   </StrictMode>,
